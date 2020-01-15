03dddaae1903d068dfe6fd665bca606ad4ec3674
xiaobxia <673806687@qq.com>
2020-01-15 16:13:35 +0800

    xg

--- src/util/domImage.js
+++ src/util/domImage.js
@@ -39,7 +39,8 @@ function convertToImage (container, options = {}) {
     ...options
   }
   return html2canvas(container, ops).then(canvas => {
-    Canvas2Image.saveAsPNG(canvas, canvas.width, canvas.height)
+    // 下载图片
+    // Canvas2Image.saveAsPNG(canvas, canvas.width, canvas.height)
     // 返回base64
     return Canvas2Image.convertToPNG(canvas, canvas.width, canvas.height)
   })
